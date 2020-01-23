09c197be26b8c8e2002f4daf91e924aa4b669f20
software_man <esyu79@gmail.com>
2020-01-23 16:43:30 +0300

    admin panel eklendi

--- Stor_Perde_Yikama/App_Start/BundleConfig.cs
+++ Stor_Perde_Yikama/App_Start/BundleConfig.cs
@@ -26,6 +26,35 @@ namespace Stor_Perde_Yikama
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //* Admin *//
+
+            bundles.Add(new StyleBundle("~/assets/css").Include(
+                      "~/assets/css/bootstrap.min.css",
+                      "~/assets/css/atlantis.min.css"));
+
+            bundles.Add(new ScriptBundle("~/assets/js/core").Include(
+                      "~/assets/js/core/jquery.3.2.1.min.js",
+                      "~/assets/js/core/popper.min.js",
+                      "~/assets/js/core/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/js/core/bootstrap").Include(
+                      "~/assets/js/core/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/js/plugin").Include(
+                      "~/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js",
+                      "~/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js",
+                      "~/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js",
+                      "~/assets/js/plugin/chart.js/chart.min.js",
+                      "~/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js",
+                      "~/assets/js/plugin/chart-circle/circles.min.js",
+                      "~/assets/js/plugin/datatables/datatables.min.js",
+                      "~/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js",
+                      "~/assets/js/plugin/jqvmap/jquery.vmap.min.js",
+                      "~/assets/js/plugin/jqvmap/maps/jquery.vmap.world.js",
+                      "~/assets/js/plugin/sweetalert/sweetalert.min.js",
+                      "~/assets/js/atlantis.min.js"
+                      ));
         }
     }
 }
