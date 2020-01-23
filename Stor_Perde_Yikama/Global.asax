69c14c5c40e958bba1171daefc2494f49baffaa8
software_man <esyu79@gmail.com>
2020-01-23 13:32:05 +0300

    Add project files.

---
+++ Stor_Perde_Yikama/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stor_Perde_Yikama.MvcApplication" Language="C#" %>
---
+++ Stor_Perde_Yikama/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stor_Perde_Yikama.Startup))]
+namespace Stor_Perde_Yikama
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
